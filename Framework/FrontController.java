181d6630f35608305ce68ec76e2ad4eb57414f49
mampydave <169129382+mampydave@users.noreply.github.com>
2024-06-14 08:31:09 +0200

    sprint6
    annotation des parametres

--- Framework/FrontController.java
+++ Framework/FrontController.java
@@ -4,10 +4,13 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 // import mg.itu.util.Mapping;
 
+import java.lang.ModuleLayer.Controller;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 import java.net.URL;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,81 +20,182 @@ import javax.print.attribute.standard.RequestingUserName;
 
 public class FrontController extends HttpServlet { 
     Map<String, Mapping> hmap;
+    Map<String, List<String>> nbParam; 
 
     @Override
     public void init() throws ServletException {
         hmap = new HashMap<>();
+        nbParam = new HashMap<>();
         try {
             ServletContext context = getServletContext();
             String chemin = context.getInitParameter("scan");
+        
             List<String> controllers = scan(chemin); 
+            boolean GetMethodPresent = false; // Flag pour vérifier les méthodes @GET
             for (String controller : controllers) {
                 Class<?> trouver = Class.forName(controller);
                 Method[] methods = trouver.getDeclaredMethods();
-                for (Method method : methods) {             
+                for (Method method : methods) {   
+                    Class<?>[] parameterTypes = method.getParameterTypes();
+                    Parameter[] parameters = method.getParameters();
+                    List<String> paramNames = new ArrayList<>();
+                    
                     if (method.isAnnotationPresent(Get.class)) {
+                        GetMethodPresent=true;
                         Object pris=trouver.getDeclaredConstructor().newInstance();
                         Get annotation = method.getAnnotation(Get.class);
                         String url = annotation.value();
-                        Mapping truest = new Mapping(trouver.getName(), method.getName(),method.invoke(pris));
-                    
-                        hmap.put(url, truest);
+                        Mapping truest;
+                        if (parameters.length>0) {
+                            Object[] arguments = new Object[parameterTypes.length];
+                            Annotation[][] parametreAnnot=method.getParameterAnnotations();
+                            for (int i = 0; i < arguments.length; i++) {
+                                Class<?> paramType = parameterTypes[i];
+                                for(Annotation getting : parametreAnnot[i]){
+                                    if (getting instanceof Param) {
+                                        Param paramAnnotation = (Param) getting;
+                                        arguments[i] = paramAnnotation.value();
+                                        paramNames.add(paramType.getName());
+                                    }
+                               }
+                                // Class<?> paramType = parameterTypes[i];
+                                // if (paramType == int.class) {
+                                //     arguments[i] = 0; 
+                                // } else if (paramType == double.class) {
+                                //     arguments[i] = 0.0; 
+                                // } else if (paramType == String.class) {
+                                //     arguments[i] = ""; 
+                                // } else {
+                                //     arguments[i] = null; 
+                                // }
+                            }
+                            nbParam.put(method.getName(), paramNames);
+                            truest = new Mapping(trouver.getName(), method.getName(),method.invoke(pris,arguments));                    
+                            hmap.put(url, truest);
+                        }else{
+
+                            truest = new Mapping(trouver.getName(), method.getName(),method.invoke(pris));
+                            if (hmap.containsKey(url)) {
+                                throw new Exception("url existant ["+ url +"] dans "+ trouver.getName() + " et "+ hmap.get(url).getClassName());
+                            }
+                            hmap.put(url, truest);
+                        }
+
                     }
                 }
+                if (!GetMethodPresent) {
+                    throw new Exception("La classe " + trouver.getName() + " n'a aucune méthode annotée avec @GET."); 
+                }
             }
+    
         } catch (Exception e) {
             throw new ServletException("Erreur lors de l'initialisation du FrontController", e);
         }
     }
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, Exception {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        try {
-            String requestUrl = request.getRequestURI().substring(request.getContextPath().length());
-            Mapping mapping = hmap.get(requestUrl);
-            out.println("<html>");
-            out.println("<head><title>Sprint2</title></head>");
-            out.println("<body>");
-            if (mapping != null) {
+       
+        String requestUrl = request.getRequestURI().substring(request.getContextPath().length());
+        String reference=requestUrl;
+        // requestUrl ="/"+requestUrl.substring(requestUrl.lastIndexOf("/") + 1);
+        Mapping mapping = hmap.get(requestUrl);
+        
 
-                Class<?> newc=Class.forName(mapping.getClassName());
-                Object controller=newc.getDeclaredConstructor().newInstance();
-                Method method=controller.getClass().getDeclaredMethod(mapping.getMethodName());
+        
+        out.println("<html>");
+        out.println("<head><title>Sprint5</title></head>");
+        out.println("<body>");
+        if (mapping != null) {
+            // out.println(mapping.getMethodName());
+
+            Class<?> newc=Class.forName(mapping.getClassName());
+            Object controller=newc.getDeclaredConstructor().newInstance();
+            Method method;
+            Object result;
+            Enumeration<String> parameterNames = request.getParameterNames();
+
+            if (parameterNames.hasMoreElements()) {
+                out.println(mapping.getMethodName());
                 
-                out.println("<h1>URL: " + requestUrl + "</h1>");
+                List<String> typeParametre= nbParam.get(mapping.getMethodName());
 
-                out.println("<li>Class: " + mapping.getClassName() + ":");
-                out.println("<ul>Method: " + mapping.getMethodName() + "</ul>");
-
-                Object result = method.invoke(controller);
-
-                if (result instanceof String) {
-                    out.println("<ul>Value of method: " + (String) result+ "</ul></li>");                                        
-                } else if (result instanceof ModelView) {
-                    ModelView model=(ModelView) result;
-                    String url=model.getUrl();
-                    HashMap<String, Object> data=model.getData();
-                    for(String key : data.keySet()){
-                        request.setAttribute(key,data.get(key));
+                    Class<?>[] pyte = new Class<?>[typeParametre.size()];
+                
+                    for (int i = 0; i < typeParametre.size(); i++) {
+                        out.println(typeParametre.get(i));
+                        try {
+                            Class<?> allParamtype=Class.forName(typeParametre.get(i));
+                            pyte[i] = allParamtype;
+                            
+                            
+                        } catch (ClassNotFoundException e) {
+                            out.println(e);
+                            pyte[i] = null;
+                        }
                     }
-                    RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-                    dispatcher.forward(request, response);
-                }else{
-                    out.println("Non reconnu");
-                }                
-
-            } else {
-                out.println("<h1>THE URL : " + requestUrl + " NOT EXIST</h1>");
+                    
+                    method=controller.getClass().getDeclaredMethod(mapping.getMethodName(),pyte);
+                    Annotation[][] parametreNotion=method.getParameterAnnotations();
+                    
+                    Class<?>[] parameterTypes = method.getParameterTypes();
+                    Object[] arguments = new Object[parameterTypes.length];
+                        
+                    int i=0;    
+                    while (parameterNames.hasMoreElements()) {
+                        String paramName = parameterNames.nextElement();
+                        String paramValue = request.getParameter(paramName);
+                        for(Annotation getting : parametreNotion[i]){
+                            if (getting instanceof Param) {
+                                Param paramAnnotation = (Param) getting;
+                                if(paramName.equalsIgnoreCase(paramAnnotation.value())){
+                                    arguments[i] = paramValue;
+                                }
+                            }
+                        }
+                        i++;
+                    }
+                result = method.invoke(controller, arguments);
+            }else{
+                method=controller.getClass().getDeclaredMethod(mapping.getMethodName());
+                result=method.invoke(controller);
             }
-            out.println("</body>");
-            out.println("</html>");
-        }catch (Exception e) {
-            System.out.println(e);
+
+            out.println("<h1>URL: " + requestUrl + "</h1>");
+            out.println("<li>Class: " + mapping.getClassName() + ":");
+            out.println("<ul>Method: " + mapping.getMethodName() + "</ul>");
+
+            if (result instanceof String) {
+                out.println("<ul>Value of method: " + (String) result+ "</ul></li>");                                        
+            } else if (result instanceof ModelView) {
+                ModelView model=(ModelView) result;
+                String url=model.getUrl();
+                HashMap<String, Object> data=model.getData();
+                for(String key : data.keySet()){
+                    request.setAttribute(key,data.get(key));
+                }
+
+                // int lastIndex = url.lastIndexOf("/");
+                // url = url.substring(0, lastIndex) + url.substring(lastIndex + 1, url.lastIndexOf("."));
+                // url = reference.substring(0, reference.lastIndexOf("/")) + url;
+
+                // int lastSlashIndex = reference.lastIndexOf("/");
+                // int secondLastSlashIndex = reference.lastIndexOf("/", lastSlashIndex - 1);
+                // url = reference.substring(0, secondLastSlashIndex + 1)+url;
+                RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+                dispatcher.forward(request, response);
+            }else {
+                throw new Exception("invalide retour ou type de retour est non reconue");
+            }                
+
+        } else {
+            out.println("<h1>THE URL : " + requestUrl + " NOT EXIST</h1>");
         }
-         finally {
-            out.close();
-        }
+        out.println("</body>");
+        out.println("</html>");
+       
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response){
@@ -106,6 +210,7 @@ public class FrontController extends HttpServlet {
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response){
         try {
+
             processRequest(request, response);
 
         } catch (Exception e) {
@@ -160,4 +265,7 @@ public class FrontController extends HttpServlet {
         return liste;
         
     }
+ 
+
+
 }
